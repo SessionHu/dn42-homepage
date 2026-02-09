634111aaf3722372628c0037b478f741d758202e
SessionHu <sessionhu@outlook.com>
2026-02-10 01:56:59 +0800

    fix(radio): playlist cgi charset

--- cgi-src/radio-playlist.sh
+++ cgi-src/radio-playlist.sh
@@ -3,7 +3,7 @@
 F=`mktemp`
 find $MUSIC_DIR -follow -type f -name '[!.]*' -exec realpath --relative-to=$MUSIC_DIR {} + | shuf > $F
 #printf "${SERVER_PROTOCOL} 200 OK\r\n"
-printf 'content-type: text/plain\r\n'
+printf 'content-type: text/plain; charset=utf-8\r\n'
 printf "content-length: $(stat -c %s $F)\r\n"
 printf '\r\n'
 cat $F
